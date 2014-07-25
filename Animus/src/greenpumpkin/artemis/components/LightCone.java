8015235f061c5bc651c09098277fc58d2bf24aec
thisisdaleb <officialgsproductions@gmail.com>
2014-07-25 05:02:53 -0500

    Continued light components

---
+++ Animus/src/greenpumpkin/artemis/components/LightCone.java
@@ -0,0 +1,8 @@
+package greenpumpkin.artemis.components;
+
+import com.artemis.Component;
+import box2dLight.ConeLight;
+
+public class LightCone extends Component {
+	ConeLight light;
+}
--- Animus/src/greenpumpkin/artemis/components/LightPoint.java
+++ Animus/src/greenpumpkin/artemis/components/LightPoint.java
@@ -3,6 +3,6 @@ package greenpumpkin.artemis.components;
 import com.artemis.Component;
 import box2dLight.PointLight;
 
-public class DotLight extends Component {
+public class LightPoint extends Component {
 	PointLight light;
 }
