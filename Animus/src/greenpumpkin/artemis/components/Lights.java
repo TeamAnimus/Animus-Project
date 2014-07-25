2e043e0a2b71657c9bcee814fbdcf63e0b680c3a
thisisdaleb <officialgsproductions@gmail.com>
2014-07-25 04:38:20 -0500

    Making components in preparation of TestWorld.

---
+++ Animus/src/greenpumpkin/artemis/components/Lights.java
@@ -0,0 +1,11 @@
+package greenpumpkin.artemis.components;
+
+import box2dLight.RayHandler;
+import com.badlogic.gdx.physics.box2d.World;
+
+public class Lights {
+	public int numRays = 8; //how many rays are emitted for shadow casting
+	public float lightDistance = 16f; // distance light goes
+	public World world; /** our box2D world, controls physics **/
+	public RayHandler rayHandler; //the main object of light2d, heavily important
+}
