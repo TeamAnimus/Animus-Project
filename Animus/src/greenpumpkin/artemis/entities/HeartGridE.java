15eadc8994d141cbcdb32db6c596f889a889bf91
texas-tiel <texas.tiel@gmail.com>
2014-07-26 23:16:47 -0500

    Added New

---
+++ Animus/src/greenpumpkin/artemis/entities/HeartGridE.java
@@ -0,0 +1,12 @@
+package greenpumpkin.artemis.entities;
+
+import com.artemis.Entity;
+import com.artemis.World;
+
+public class HeartGridE {
+	
+	public static Entity create(World world) {
+		Entity e = world.createEntity();
+		return e;
+	}
+}
---
+++ Animus/src/greenpumpkin/artemis/entities/WeaponE.java
@@ -0,0 +1,12 @@
+package greenpumpkin.artemis.entities;
+
+import com.artemis.Entity;
+import com.artemis.World;
+
+public class WeaponE {
+	
+	public static Entity create(World world) {
+		Entity e = world.createEntity();
+		return e;
+	}
+}
