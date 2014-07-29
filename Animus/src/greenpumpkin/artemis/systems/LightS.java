e282efe6b821d9cfb893e57cfd06924284f2fe3c
thisisdaleb <officialgsproductions@gmail.com>
2014-07-29 01:30:12 -0500

    Added inserted and removed methods, and hashmap

--- Animus/src/greenpumpkin/artemis/systems/LightS.java
+++ Animus/src/greenpumpkin/artemis/systems/LightS.java
@@ -1,29 +1,46 @@
 package greenpumpkin.artemis.systems;
 
+import java.util.HashMap;
+
 import greenpumpkin.artemis.components.LightC;
 import greenpumpkin.artemis.entities.LightFactory;
 import greenpumpkin.artemis.units.AnimusWorld;
-import box2dLight.RayHandler;
+import box2dLight.Light;
 import com.artemis.Aspect;
 import com.artemis.ComponentMapper;
 import com.artemis.Entity;
-import com.artemis.EntitySystem;
 import com.artemis.annotations.Mapper;
+import com.artemis.systems.IntervalEntitySystem;
 import com.artemis.utils.ImmutableBag;
 import com.badlogic.gdx.graphics.Color;
 
-public class LightS extends EntitySystem {
+public class LightS extends IntervalEntitySystem {
 	@Mapper ComponentMapper<LightC> lightMap;
+	
+	HashMap<Integer, Light> lightIndex = new HashMap<Integer, Light>();
 
 	@SuppressWarnings("unchecked")
 	public LightS() {
-		super(Aspect.getAspectForAll(LightC.class));
+		super(Aspect.getAspectForAll(LightC.class), 1/60.0f);
 	}
 	
 	protected void initialize(){
 		LightFactory.createPoint(world, AnimusWorld.rayHandler,  AnimusWorld.numRays, new Color(0.0f, 0.0f, 0.6f, 1.0f),  AnimusWorld.lightDistance*4, 6f, 1f).addToWorld();
 		LightFactory.createPoint(world, AnimusWorld.rayHandler,  AnimusWorld.numRays, new Color(1.0f, 1.0f, 0.8f, 1.0f),  AnimusWorld.lightDistance*2, 35f, 11f).addToWorld();
-		//the real list of lights will be created with a for loop where the numbers come from a JSON file.
+		//the real list of lights will be created with a for loop where the numbers come from a JSON file, in a different system.
+	}
+	
+	@Override
+	protected void inserted(Entity e) {
+		LightC newLight = lightMap.get(e);
+		lightIndex.put(e.getId(), newLight.light);
+	}
+	
+	@Override
+	protected void removed(Entity e) {
+		if (lightIndex.containsKey(e.getId())) {
+			lightIndex.get(e.getId()).remove();
+		}
 	}
 
 	@Override
