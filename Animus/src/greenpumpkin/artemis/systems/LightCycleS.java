05db93e51cda8e589459235792d9418945a53de1
thisisdaleb <officialgsproductions@gmail.com>
2014-07-30 13:24:00 -0500

    Reorganized LightCycle code

--- Animus/src/greenpumpkin/artemis/systems/LightCycleS.java
+++ Animus/src/greenpumpkin/artemis/systems/LightCycleS.java
@@ -2,45 +2,68 @@ package greenpumpkin.artemis.systems;
 
 import greenpumpkin.artemis.components.LightC;
 import greenpumpkin.artemis.components.LightCycleC;
-import greenpumpkin.artemis.components.PositionC;
 import com.artemis.Aspect;
 import com.artemis.ComponentMapper;
 import com.artemis.Entity;
 import com.artemis.annotations.Mapper;
 import com.artemis.systems.IntervalEntityProcessingSystem;
 
-public class LightCycleS extends IntervalEntityProcessingSystem {//Eric
-	@Mapper//Eric
-	ComponentMapper<LightC> lightMap;//Eric
-	@Mapper//Eric
-	ComponentMapper<LightCycleC> cycleMap;//Eric
-	//Eric
-	@SuppressWarnings("unchecked")//Eric
-	public LightCycleS() {//Eric
-		super(Aspect.getAspectForAll(LightC.class, LightCycleC.class), 1 / 60f);//Eric
-	}//Eric
-	//Eric
-	@Override//Eric
-	protected void process(Entity e) {//Eric
-		LightC newLight = lightMap.get(e);//Eric
+public class LightCycleS extends IntervalEntityProcessingSystem {
+	@Mapper
+	ComponentMapper<LightC> lightMap;
+	@Mapper
+	ComponentMapper<LightCycleC> cycleMap;
+	
+	@SuppressWarnings("unchecked")
+	public LightCycleS() {
+		super(Aspect.getAspectForAll(LightC.class, LightCycleC.class), 1 / 30f);
+	}
+	
+	@Override
+	protected void process(Entity e) {
+		LightC newLight = lightMap.get(e);
 		LightCycleC cycle = cycleMap.get(e);
-		//Eric
-		//if((1/2f) > cycle.currTime) {cycle.currTime += 1 / 60f;}//Eric
-		if ((cycle.time / 2) > cycle.currTime) {//Eric
-			newLight.light.setDistance(newLight.light.getDistance() + (cycle.size / 60.0f / cycle.time));//Eric
-			cycle.currTime += 1 / 60f;//Eric
+		
+		
+		if ((cycle.time / 8) > cycle.currTime) {
+			increaseSlow(newLight, cycle);
 		}
-		//else if((cycle.time / 2f + (1/4f)) > cycle.currTime){cycle.currTime += 1 / 60f;}
-		//Eric
-		else {//Eric
-			newLight.light.setDistance(newLight.light.getDistance() - (cycle.size / 60.0f / cycle.time));//Eric
-			cycle.currTime += 1 / 60f;//Eric
-		}//Eric
-		//Eric
-		if (cycle.time < cycle.currTime) {//Eric
-			cycle.currTime = 0;//Eric
+		else if((cycle.time / 8*3) > cycle.currTime){
+			increaseFast(newLight, cycle);
+		}
+		else if((cycle.time / 2) > cycle.currTime){
+			increaseSlow(newLight, cycle);
+		}
+		else if((cycle.time / 8*5) > cycle.currTime){
+			decreaseSlow(newLight, cycle);
+		}
+		else if((cycle.time / 8*7) > cycle.currTime){
+			decreaseFast(newLight, cycle);
+		}
+		else if (cycle.time > cycle.currTime) {
+			decreaseSlow(newLight, cycle);
+		}
+		else {
+			System.out.println("difference: " + (newLight.light.getDistance() - (cycle.maxDist-cycle.size)));
+			cycle.currTime = 0;
 			newLight.light.setDistance(cycle.maxDist-cycle.size);
-		}//Eric
-		//Eric
-	}//Eric
-}//Plot Twist: Daleb
+		}
+	}
+	
+	protected void increaseFast(LightC newLight, LightCycleC cycle) {
+		newLight.light.setDistance(newLight.light.getDistance() + (cycle.size*2f / 30.0f / cycle.time));
+		cycle.currTime += 1 / 30f;
+	}
+	protected void increaseSlow(LightC newLight, LightCycleC cycle) {
+		newLight.light.setDistance(newLight.light.getDistance() + (cycle.size/2f / 30.0f / cycle.time));
+		cycle.currTime += 1 / 30f;
+	}
+	protected void decreaseFast(LightC newLight, LightCycleC cycle) {
+		newLight.light.setDistance(newLight.light.getDistance() - (cycle.size*2f / 30.0f / cycle.time));
+		cycle.currTime += 1 / 30f;
+	}
+	protected void decreaseSlow(LightC newLight, LightCycleC cycle) {
+		newLight.light.setDistance(newLight.light.getDistance() - (cycle.size/2f / 30.0f / cycle.time));
+		cycle.currTime += 1 / 30f;
+	}
+}
